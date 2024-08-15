051ad862ce140bd6674dd481a4065597cc1f5506
Jesanma <64873639+jesanma97@users.noreply.github.com>
2024-08-15 14:33:00 +0200

    Create entities and H2 structure (#1)
    * Creation project
    
    * Create entities, schema and data initial

---
+++ src/main/java/com/inditex/RestServicePriceProduct/infrastructure/adapters/out/persistence/entities/BrandEntity.java
@@ -0,0 +1,22 @@
+package com.inditex.RestServicePriceProduct.infrastructure.adapters.out.persistence.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "BRAND")
+public class BrandEntity {
+    @Id
+    private Long id;
+
+    @Column(name = "BRAND_NAME")
+    private String nameBrand;
+
+    @OneToMany(mappedBy = "brand", cascade = CascadeType.ALL)
+    private List<PriceEntity> prices;
+}
---
+++ src/main/java/com/inditex/RestServicePriceProduct/infrastructure/commons/Constants.java
@@ -0,0 +1,10 @@
+package com.inditex.RestServicePriceProduct.infrastructure.commons;
+
+public class Constants {
+
+    public enum Currency {
+        EUR,
+        USD,
+        GBP
+    }
+}
