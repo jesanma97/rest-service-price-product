8a64557be78b93a8abd06ac11ad16979dea8c2bb
Jesús Ssngonzalo Martínez <jesanma97@gmail.com>
2024-08-15 13:41:54 +0200

    Creation project

---
+++ src/test/java/com/inditex/RestServicePriceProduct/RestServicePriceProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inditex.RestServicePriceProduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServicePriceProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
