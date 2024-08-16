3ed2522ce91800c3d891841ae6754ac6db70d909
Jesanma <64873639+jesanma97@users.noreply.github.com>
2024-08-16 17:26:41 +0200

    Creation hexagonal architecture components, formatting localdatetime, query to get price with high priority (#4)

--- src/main/java/com/inditex/RestServicePriceProduct/infrastructure/adapters/out/persistence/mappers/PriceMapperEntity.java
+++ src/main/java/com/inditex/RestServicePriceProduct/infrastructure/adapters/out/persistence/mappers/PriceMapperEntity.java
@@ -7,15 +7,19 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
+import java.util.List;
+
 @Mapper
 public interface PriceMapperEntity {
     PriceMapperEntity INSTANCE = Mappers.getMapper(PriceMapperEntity.class);
 
     @Mapping(source = "brand.id", target = "idBrand")
     @Mapping(source = "product.id", target = "idProduct")
-    PriceResponse toDomain(PriceEntity priceEntity);
+    PriceResponse priceEntityToPriceResponse(PriceEntity priceEntity);
 
     @Mapping(source = "idBrand", target = "brand.id")
     @Mapping(source = "idProduct", target = "product.id")
-    PriceEntity toDomain(PriceRequest priceRequest);
+    PriceEntity priceRequestToPriceEntity(PriceRequest priceRequest);
+
+    List<PriceResponse> listPriceEntityToListPriceResponse(List<PriceEntity> priceEntityList);
 }
