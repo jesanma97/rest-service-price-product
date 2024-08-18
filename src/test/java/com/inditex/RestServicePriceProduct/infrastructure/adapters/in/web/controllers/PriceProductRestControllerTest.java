ca7348754b65ae8b9c0d4b512b87daebc02b719b
Jesús Ssngonzalo Martínez <jesanma97@gmail.com>
2024-08-18 17:52:58 +0200

    Refactor ruta controller

--- src/test/java/com/inditex/RestServicePriceProduct/infrastructure/adapters/in/web/controllers/PriceProductRestControllerTest.java
+++ src/test/java/com/inditex/RestServicePriceProduct/infrastructure/adapters/in/web/controllers/PriceProductRestControllerTest.java
@@ -1,10 +1,8 @@
-package com.inditex.RestServicePriceProduct.infrastructure.config.controllers;
+package com.inditex.RestServicePriceProduct.infrastructure.adapters.in.web.controllers;
 
 import com.inditex.RestServicePriceProduct.domain.PriceRequest;
 import com.inditex.RestServicePriceProduct.domain.PriceResponse;
 import com.inditex.RestServicePriceProduct.infrastructure.adapters.in.web.PriceProductConsultAdapter;
-import com.inditex.RestServicePriceProduct.infrastructure.adapters.in.web.controllers.PriceProductRestController;
-import jakarta.inject.Inject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
